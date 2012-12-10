97d980b43584e523fd894e594bb531b692fe0a25
Marcin Czerwiński <marcinczrw@gmail.com>
2012-12-10 15:28:24 +0100

    cos dodam

---
+++ SpadajaceLiterki/src/Algorithm.java
@@ -0,0 +1,29 @@
+
+public class Algorithm {
+
+	public static char[][] cloneArray(char[][] src) {
+	    int length = src.length;
+	    char[][] target = new char[length][src[0].length];
+	    for (int i = 0; i < length; i++) {
+	        System.arraycopy(src[i], 0, target[i], 0, src[i].length);
+	    }
+	    return target;
+	}
+	public static char[][] removePair(char[][] T, int Y1, int X1, int Y2, int X2)
+	{
+		for(;Y1>0;Y1--)
+		{
+			T[Y1][X1]=T[Y1-1][X1];
+		}
+		T[0][X1]=' ';
+		
+		for(;Y2>0;Y2--)
+		{
+			T[Y2][X2]=T[Y2-1][X2];
+		}
+		T[0][X2]=' ';
+		
+		return T;
+	}
+
+}
---
+++ SpadajaceLiterki/src/LetterTable.java
@@ -0,0 +1,164 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class LetterTable {
+	/*
+	 * N-wysokosc
+	 * M-szerokosc
+	 */
+	
+	
+	//tworzy pusta tablice literek NxM
+	public void createEmpty(int N, int M)
+	{
+		Width = N;
+		Height = M;
+		Size = N*M;
+	}
+	
+	//generuje tablice literek NxM zapeniona literkami
+	public void generate(int N, int M)
+	{
+		Table = new char[N][M];
+		Width = N;
+		Height = M;
+		Size = N*M;
+		
+		if((N*M)%2!=0)
+		{
+			System.out.println("Zle wymiary");
+			return;
+		}
+		
+		
+		ArrayList<Integer> Positions=new ArrayList<Integer>();//z tego losujemy gdzie wstawiamy literki
+		
+		for(int i=0; i < Size; i++)
+		{
+			Positions.add(new Integer(i));
+		}
+		
+		char Letter;
+		int Position;
+		Random Generator=new Random(System.currentTimeMillis());
+		
+		//petla po wszystkich pozycjach
+		while(!Positions.isEmpty())
+		{
+			Letter ='A';
+			Letter += Generator.nextInt(26);
+			
+			//bierzemy jeden z numerow ktory pozostal na liscie pozycji
+			Position=Positions.remove(Generator.nextInt(Positions.size()));
+			Table[(Position/Height)][(Position%Height)]=Letter;
+			
+			//powtarzamy
+			Position=Positions.remove(Generator.nextInt(Positions.size()));
+			Table[(Position/Height)][(Position%Height)]=Letter;
+		}
+		
+	}
+	public void print()
+	{
+		for(int i=0;i<Width;i++)
+		{
+			for(int j=0;j<Height;j++)
+			{
+				System.out.print(Table[i][j]);
+			}
+			System.out.println();
+		}
+	}
+	//liczy ile jest mozliwych par do wybrania
+	public int calcPairs()
+	{
+		
+		for(int i=0;i<Width;i++)
+		{
+			for(int j=0;j<Height;j++)
+			{
+				int iTemp=i+1;
+				int jTemp=j+1;
+				for(;iTemp<Width;iTemp++)
+				{
+					if(Table[i][j]==Table[iTemp][j]) Pairs++;
+				}
+				for(;jTemp<Height;jTemp++)
+				{
+					if(Table[i][j]==Table[i][jTemp]) Pairs++;
+				}
+			}
+			
+		}
+		return Pairs;
+	}
+	//zwraca wektor wszystkich mozliwych tablic po usunieciu pary
+	public ArrayList<LetterTable> getNextTables()
+	{
+		ArrayList<LetterTable> Tables=new ArrayList<LetterTable>();
+		char[][] TableTemp;
+		
+		//szukanie par
+		for(int i=0;i<Width;i++)
+		{
+			for(int j=0;j<Height;j++)
+			{
+				int iTemp=i+1;
+				int jTemp=j+1;
+				for(;iTemp<Width;iTemp++)
+				{
+					//jesli znajdzie pare w poziomie
+					if(Table[i][j]==Table[iTemp][j])
+					{
+						//tworzy nowa tablice char[][] i usuwa pare el.
+						TableTemp=Algorithm.cloneArray(Table);
+						Algorithm.removePair(TableTemp, i, j, iTemp, j);
+						
+						//tworzy nowa tablice liter i dodaje do listy
+						LetterTable LTableTemp=new LetterTable();
+						LTableTemp.createEmpty(Width, Height);
+						LTableTemp.setLettersNumber(LettersNumber-2);
+						LTableTemp.setTable(TableTemp);
+						Tables.add(LTableTemp);
+					}
+				}
+				for(;jTemp<Height;jTemp++)
+				{
+					//jesli znajdzie w pionie
+					if(Table[i][j]==Table[i][jTemp]) 
+					{
+						//tworzy nowa tablice char[][] i usuwa pare el.
+						TableTemp=Algorithm.cloneArray(Table);
+						Algorithm.removePair(TableTemp, i, j, i, jTemp);
+						
+						//tworzy nowa tablice liter i dodaje do listy
+						LetterTable LTableTemp=new LetterTable();
+						LTableTemp.createEmpty(Width, Height);
+						LTableTemp.setLettersNumber(LettersNumber-2);
+						LTableTemp.setTable(TableTemp);
+						Tables.add(LTableTemp);
+					}
+				}
+			}
+			
+		}
+		return Tables;
+	}
+	public void setTable(char[][] T)
+	{
+		Table=T;
+	}
+	public void setLettersNumber(int L)
+	{
+		LettersNumber=L;
+	}
+	
+	private int LettersNumber;
+	private int Pairs=0;
+	private int Size;
+	private int Width;
+	private int Height;
+	private char[][] Table;
+	
+}
